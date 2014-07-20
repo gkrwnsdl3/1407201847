bbe9c2314821d0bbac5650af792c9a89626a2ab4
Home <TNWL>
2014-07-20 22:08:05 +0900

    a

--- 140720/src/main.java
+++ 140720/src/main.java
@@ -3,7 +3,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Sun 140720 2200");
+		System.out.println("2207");
 	}
 
 }
